c122b1af2008c9c736edb6d5b9c8f9e1b1c60c23
Paul Zhang <paul.zhang4156@gmail.com>
2019-07-02 05:32:41 +1000

    1 Setup ESLint with airbnb style

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
 const log4js = require('log4js');
 const { basicLayout } = require('log4js/lib/layouts');
 
-const match = (key, { exact = [], regex = [] }) => exact.find(matcher => key.toLowerCase() === matcher.toLowerCase()) !== undefined ||
-  regex.find(matcher => key.match(new RegExp(matcher, 'i'))) !== undefined;
+/**
+ * Test key string against matchers
+ *
+ * @param {string} key
+ * @param {array} exact
+ * @param {array} regex
+ *
+ * @returns {boolean} true if matches and therefore should be filtered
+ */
+const match = (key, { exact = [], regex = [] }) => exact.find(
+  matcher => key.toLowerCase() === matcher.toLowerCase(),
+) !== undefined
+  || regex.find(matcher => key.match(new RegExp(matcher, 'i'))) !== undefined;
 
+/**
+ * Recursively filter log data.
+ * If data is of simple type, return as is.
+ * If data is an array, filter each element.
+ * If data is an object, filter against fields. If not filtered, filter each value.
+ *
+ * @param {any} data
+ * @param {object} config
+ *
+ * @returns {any}
+ */
 const filter = (data, config) => {
-
   if (!data || typeof data !== 'object') {
     return data;
   }
@@ -14,20 +35,31 @@ const filter = (data, config) => {
     return data.map(entry => filter(entry, config));
   }
 
-  return Object.entries(data).reduce((prev, [key, value]) => {
-    if (match(key, config)) {
-      return { ...prev, [key]: '******' };
-    }
-    return { ...prev, [key]: filter(value, config) };
-  }, {});
+  return Object.entries(data)
+    .reduce((prev, [key, value]) => {
+      if (match(key, config)) {
+        return {
+          ...prev, [key]: '******',
+        };
+      }
+
+      return {
+        ...prev, [key]: filter(value, config),
+      };
+    }, {});
 };
 
-log4js.addLayout('log4js-filters', (config) => (log) => {
+// Add filters as a new layout. No side effect unless one starts to use it.
+log4js.addLayout('log4js-filters', config => (log) => {
   const { data } = log;
-  const newLog = { ...log, data: filter(data, config) };
+  const newLog = {
+    ...log, data: filter(data, config),
+  };
+
   return basicLayout(newLog);
 });
 
+// Start with empty matchers.
 const layout = {
   type: 'log4js-filters',
   exact: [],
